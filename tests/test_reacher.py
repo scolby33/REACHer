09881a5b95af7069d8990e770d8e78f2104a42b2
Scott Colby <scolby33@gmail.com>
2019-01-23 11:53:05 -0500

    Add tests stubs

---
+++ tests/test_reacher.py
@@ -0,0 +1,11 @@
+"""Test the non-web-based functionality of REACHer."""
+
+from reacher import extract_abstract, print_reach_statistics
+
+
+def test_extract_abstract():
+    pass
+
+
+def test_print_reach_statistics():
+    pass
