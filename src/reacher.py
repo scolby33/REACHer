f5d1a393142895060bdec88822d25b4c3f7d84ff
Scott Colby <scolby33@gmail.com>
2019-01-23 11:05:09 -0500

    Add docstrings

--- src/reacher.py
+++ src/reacher.py
@@ -1,3 +1,7 @@
+"""REACHer.
+
+Download an abstract from PubMed with its PMID, extract mentioned pathways with Reach, and save the output JSON.
+"""
 import sys
 from typing import Optional
 
@@ -9,12 +13,27 @@ REACH_URI = 'http://agathon.sista.arizona.edu:8080/odinweb/api/text'
 
 
 def download_pmid_xml(pmid: str) -> str:
+    """Retrieve the XML data record for the given PMID
+
+    (Due to the Entrez API, passing multiple IDs separated by commas in a single string
+    will retrieve the records for all of the IDs.)
+
+    :param pmid: the PubMed ID of the record to be retrieved
+
+    :returns: the XML data record as a string
+    """
     resp = requests.get(BASE_PUBMED_URI.format(PMID=pmid))
     resp.raise_for_status()
     return resp.text
 
 
 def extract_abstract(xml: str) -> Optional[str]:
+    """Extract all the abstract text from an Entrez XML data record
+
+    :param xml: the XML data record as a string
+
+    :returns: all the abstract text
+    """
     root = fromstring(xml)
 
     abstract_texts = []
@@ -27,6 +46,12 @@ def extract_abstract(xml: str) -> Optional[str]:
 
 
 def reach_extract(text: str) -> str:
+    """Provide text to Reach to be processed and return the JSON string of the results
+
+    :param text: the text string to be processed by Reach
+
+    :returns: the resulting Reach JSON ('fries' output format) as a string
+    """
     resp = requests.post(REACH_URI, data={'text': text, 'output': 'fries'})
     resp.raise_for_status()
     return resp.text
